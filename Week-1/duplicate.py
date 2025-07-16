9583a1b98885ca22e77ed66776e38e1fd33620aa
Najuhilal <najumv36@gmail.com>
2025-07-16 11:15:45 +0530

    Add files via upload

---
+++ Week-1/duplicate.py
@@ -0,0 +1,8 @@
+list1=[20,20,10,30,30]
+print("Duplicated List:", list1)
+list2=[]
+for i in list1:
+    if i not in list2:
+        list2.append(i)
+        print("Deduplicated List:", list2)
+    
