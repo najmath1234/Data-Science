83b4bd4d84943f0bd58f595e6136c7a947d193b1
Najuhilal <najumv36@gmail.com>
2025-09-18 15:05:15 +0530

    Add files via upload

---
+++ Week-3/plot-scatterplot.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot
+
+rollnos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+marks = [22,87,5,43,56,73,55,54,11,20,51,5,79,31,27]
+
+fig, ax = pyplot.subplots(1, 1)
+ax.scatter(rollnos, marks)
+ax.set_title("Scatter Plot")
+ax.set_xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+ax.set_yticks([25,50,75,100])
+ax.set_xlabel('rollnos')
+ax.set_ylabel('marks')
+
+pyplot.show()
